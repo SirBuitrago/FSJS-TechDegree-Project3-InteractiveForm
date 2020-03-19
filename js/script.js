c88a66e7270d0357185ec35080be9a33f963a2c3
Brian Buitrago <b.buitrago@hotmail.com>
2020-03-18 20:14:30 -0400

    writing code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/******************************************
+Brian Buitrago
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+******************************************/
+
+"use strict";
+
+// This is the variable and method to grab the "name" text field. It's meant to have the field selected and ready to type by default- when someone first arrives to the page or refreshes it.
+const input = document.getElementById("name");
+input.focus();
