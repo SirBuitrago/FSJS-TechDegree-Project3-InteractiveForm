d75b7decaae13d8858d99d6c48b0724c60e1ee9f
Brian Buitrago <b.buitrago@hotmail.com>
2020-03-31 17:44:26 -0400

    still coding...

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ FSJS project 3 - Interactive Form
 
 "use strict";
 
-// This is the variable and method to grab the "name" text field. It's meant to have the field selected and ready to type by default- when someone first arrives to the page or refreshes it.
-const input = document.getElementById("name");
-input.focus();
+//This is the variable and method to grab the "name" text field. It's meant to have the field selected and ready to type by default- when someone first arrives to the page or refreshes it.
+const inputName = document.getElementById("name");
+inputName.focus();
+
+//Here I create a function, event listener and if statement to control the flow of the drop down list and what options are hidden, unless a specific option is selected- in the previous list.
+
+const tSize = document.querySelectorAll("#size option");
+//const tDesign = document.querySelectorAll("#theme");
+const tColors = document.querySelectorAll("colors");
+const tDesign = document.getElementById("theme");
+
+tDesign.hidden = true;
+
+console.log(tDesign);
+console.log(tSize);
+
+//Thank you for taking a look at my code. I am going for the "Exceeds Expectations" grade. If its not on par with that grade, then please reject this project for resubmission.
