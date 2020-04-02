a2ea6635144e649bf4aaa45dc95b8e52add14dfe
Brian Buitrago <b.buitrago@hotmail.com>
2020-04-02 19:06:29 -0400

    still in the works..

--- js/script.js
+++ js/script.js
@@ -10,16 +10,28 @@ FSJS project 3 - Interactive Form
 const inputName = document.getElementById("name");
 inputName.focus();
 
-//Here I create a function, event listener and if statement to control the flow of the drop down list and what options are hidden, unless a specific option is selected- in the previous list.
+//Here I declare variables that contain elements from the HTML file, create a function, event listener and if statement to control the flow of the drop down list and what options are hidden, unless a specific option is selected- in the previous list.
 
 const tSize = document.querySelectorAll("#size option");
-//const tDesign = document.querySelectorAll("#theme");
 const tColors = document.querySelectorAll("colors");
-const tDesign = document.querySelector('option[value="js puns"]');
+const jsPuns = document.querySelector('option[value="js puns"]');
+const heartJS = document.querySelector('option[value="heart js"]');
+const punsColors = document.querySelectorAll("#jsp");
+const heartsColors = document.querySelectorAll("#hjs");
 
-tDesign.hidden = true;
+//Hiding each color element from the indicated drop list using a for loop.
 
-console.log(tDesign);
+for (let i = 0; i < punsColors.length; i++) {
+	punsColors[i].hidden = true;
+	heartsColors[i].hidden = true;
+}
+
+//Logging declared variables to the console, to verify desired output.
+console.log(jsPuns);
 console.log(tSize);
+console.log(heartJS);
+console.log(punsColors);
+console.log(heartsColors);
+console.log(tColors);
 
 //Thank you for taking a look at my code. I am going for the "Exceeds Expectations" grade. If its not on par with that grade, then please reject this project for resubmission.
