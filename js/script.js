57eb16992663fbc0f77c6b9add52f5597918489f
Brian Buitrago <b.buitrago@hotmail.com>
2020-04-12 20:14:36 -0400

    still coding..

--- js/script.js
+++ js/script.js
@@ -21,32 +21,59 @@ const tDesign = document.querySelector("#design");
 const jsPuns = document.querySelector('option[value="js puns"]').value;
 const heartJS = document.querySelector('option[value="heart js"]').value;
 
-//Hiding each color element from the indicated drop list using a for loop.
-for (let i = 0; i < 6; i++) {
-	tColors[i].hidden = true;
+//Hiding each color element from the indicated drop list using a for loop within a function.
+function hideColors() {
+	for (let i = 0; i < 6; i++) {
+		tColors[i].hidden = true;
+	}
 }
+hideColors();
 //Event listener that determines which elements to show in the "color" droplist according to what is chosen in the previous drop list.
-tDesign.addEventListener("change", event => {
+
+tDesign.addEventListener("change", (e) => {
 	for (let i = 0; i < 3; i++) {
-		let eventValue = event.target.value;
+		let eventValue = e.target.value;
 
 		if (eventValue === jsPuns) {
+			punsColors[0].selected = true;
 			punsColors[i].hidden = false;
 			heartsColors[i].hidden = true;
 		} else if (eventValue === heartJS) {
+			heartsColors[0].selected = true;
 			heartsColors[i].hidden = false;
 			punsColors[i].hidden = true;
+		} else {
+			hideColors();
 		}
 	}
 
 	console.log("This event works!");
 });
 
-//Logging declared variables to the console, to verify desired output.
-console.log(jsPuns);
-console.log(heartJS);
-console.log(tSize);
-console.log(punsColors);
-console.log(heartsColors);
-console.log(tColors);
-console.log(tDesign);
+//Logging declared variables to the console, to verify desired output.-Register for Activities section
+const checkboxes = document.querySelectorAll(".activities input");
+const checkboxOptions = document.querySelector(".activities");
+
+//Event listener for disabling the chekc boxes that overlap with a similar time slot conf. The for loop is to iterate over the checkboxes and determine their same type, for said disabling. I also add the sum of the checkbox options that are clicked and print them to page.
+checkboxOptions.addEventListener("change", (e) => {
+	let costTotal = 0;
+	let clicked = e.target;
+	let clickedCost = clicked.getAttribute("data-cost");
+	const clickedType = clicked.getAttribute("data-day-and-time");
+
+	for (let i = 0; i < checkboxes.length; i++) {
+		let checkBoxIteration = checkboxes[i];
+
+		const checkboxType = checkboxes[i].getAttribute("data-day-and-time");
+
+		if (clickedType === checkboxType && clicked !== checkBoxIteration) {
+			if ((clicked.checked = true || false)) {
+				checkBoxIteration.disabled = true;
+			} else {
+				checkBoxIteration.disabled = false;
+			}
+		}
+	}
+	console.log(clickedCost);
+	console.log("This click event is functional");
+});
